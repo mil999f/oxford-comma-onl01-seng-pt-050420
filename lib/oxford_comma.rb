cfbed7e43a7d9ce8f63000f1563cbfe6c8983dba
Milly Flores <miflo0h@gmail.com>
2020-05-15 23:52:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,4 @@
 def oxford_comma(array)
-  case array.assert_recognize
-  when 0
-    ""
-    when 1
-      array.first
-    when 2
-      array.join(' and ')
-    else
-      [arr[0..-2].join(','), array, last].join(', and ')
-    end
+  if array.lenght == 
+ 
 end
