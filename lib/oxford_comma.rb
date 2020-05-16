300c8f70f3bc0a7232aa0b6b7cd887e305aa57d6
Milly Flores <miflo0h@gmail.com>
2020-05-16 00:39:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+  return array.first if array.length == 1
+  if array.length == 2
+     array.insert(1,"and").join(" ")
+ elsif array.length > 2 
+  array[-1].insert(0, "and ")
+  array.join(", ")
+  end  
+end
 
+
+def of(arry)
+  if array.length == 1 
+    array.join
+    elsif array.lenth == 2 
+    array[0] << (" and ")
+    array.join
+  else array.length > 2 
+    array[-1].prepend "and "
+    array.join(", ")
+  end
 end
