b3c89e9e70eb01e64a09cbd1360fa5fa39682ffa
Milly Flores <miflo0h@gmail.com>
2020-05-16 00:30:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length == 2
      array.insert(1,"and").join(" ")
  elsif array.length > 2 
-  array.insert(-2, "and ")
+  array[-1].insert(0, "and ")
   array.join(", ")
   end  
 end
