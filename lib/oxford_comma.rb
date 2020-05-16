2eba38ddc7c9043142a77c1445ff1c5738cd3385
Milly Flores <miflo0h@gmail.com>
2020-05-16 00:29:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
   return array.first if array.length == 1
   if array.length == 2
+    array = boy girl
      array.insert(1,"and").join(" ")
  elsif array.length > 2 
-  array[-1].insert(0, "and ")
+  array.insert(-2, "and ")
   array.join(", ")
   end  
 end
